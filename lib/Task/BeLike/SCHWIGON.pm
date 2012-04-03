ebf1a532982cf08a0f5bc53f62269a8803118235
Steffen Schwigon <ss5@renormalist.net>
2012-04-03 12:05:08 +0200

    be a bit like FLORA

---
+++ lib/Task/BeLike/SCHWIGON.pm
@@ -0,0 +1,281 @@
+use strict;
+use warnings;
+
+package Task::BeLike::SCHWIGON;
+# ABSTRACT: Modules SCHWIGON uses
+
+=pkgroup Applications
+
+=pkg App::Ack
+
+=pkg App::Metabase::Relayd
+
+=pkg App::Nopaste
+
+=pkg App::Software::License
+
+=pkg App::cpanminus
+
+=pkg App::cpanoutdated
+
+=pkg CPAN::Mini
+
+=pkg CPAN::Mini::Devel
+
+=pkg CPAN::Uploader
+
+=pkgroup Optional Dependencies Of Other Modules
+
+=pkg CPAN::Reporter
+
+=pkg PadWalker
+
+=pkg Task::SDK::Moose
+
+=pkg Term::ReadLine::Gnu
+
+=pkg Test::Reporter
+
+=pkg Test::Reporter::Transport::Socket
+
+=pkgroup Development Tools
+
+=pkg Acme::MetaSyntactic
+
+=pkg Carp::Always
+
+=pkg Carp::Always::Color
+
+=pkg Carp::REPL
+
+=pkg Devel::bt
+
+=pkg Devel::Cover
+
+=pkg Devel::NYTProf
+
+=pkg Dist::Zilla
+
+=pkg Dist::Zilla::PluginBundle::SCHWIGON
+
+=pkg Dist::Zilla::App::Command::weaverconf
+
+=pkg Gitalist
+
+=pkg Module::CPANTS::Analyse
+
+=pkg Module::Install 0.95
+
+=pkg Module::Install::AuthorRequires
+
+=pkg Module::Install::AuthorTests
+
+=pkg Module::Install::CheckConflicts
+
+=pkg Module::Install::ExtraTests
+
+=pkg Perl::Tidy
+
+=pkg Perl::Version 1.010
+
+=pkg Pod::Coverage::TrustPod
+
+=pkg Test::Aggregate
+
+=pkg Test::Pod
+
+=pkg Test::Pod::Coverage
+
+=pkg Test::Without::Module
+
+=pkg V
+
+=pkgroup Modules I use a lot
+
+=pkg aliased
+
+=pkg App::Cmd
+
+=pkg Bundle::CPANxxl
+
+=pkg Bundle::libnet
+
+=pkg Bundle::LWP
+
+=pkg Cache::Cache
+
+=pkg Cache::FileCache
+
+=pkg Catalyst::Model::KiokuDB
+
+=pkg Data::DPath
+
+=pkg Data::Visitor
+
+=pkg Data::YAML
+
+=pkg DateTime
+
+=pkg DateTime::Format::Builder
+
+=pkg DateTime::Format::Mail
+
+=pkg DateTime::Format::MySQL
+
+=pkg DateTime::Format::Natural
+
+=pkg DateTime::Format::Strptime
+
+=pkg DBD::mysql
+
+=pkg DBD::SQLite
+
+=pkg DBI
+
+=pkg DBIx::Class
+
+=pkg DBIx::Class::Schema::Loader
+
+=pkg DBIx::Class::Schema::Versioned
+
+=pkg DBIx::Class::TimeStamp
+
+=pkg Devel::Backtrace
+
+=pkg Digest::SHA1
+
+=pkg Directory::Scratch
+
+=pkg File::ShareDir
+
+=pkg File::Slurp
+
+=pkg File::Type
+
+=pkg Function::Parameters
+
+=pkg Hash::Merge
+
+=pkg Hash::Merge::Simple
+
+=pkg JSON
+
+=pkg JSON::Syck
+
+=pkg JSON::XS
+
+=pkg List::AllUtils
+
+=pkg List::MoreUtils
+
+=pkg Log::Log4perl
+
+=pkg Method::Signatures
+
+=pkg Method::Signatures::Simple
+
+=pkg MIME::Lite
+
+=pkg MIME::Tools
+
+=pkg MIME::Types
+
+=pkg Module::Install
+
+=pkg Module::Starter
+
+=pkg Moose
+
+=pkg Moose::Autobox
+
+=pkg MooseX::Daemonize
+
+=pkg MooseX::Declare
+
+=pkg MooseX::LazyRequire
+
+=pkg MooseX::Log::Log4perl
+
+=pkg MooseX::Method::Signatures
+
+=pkg MooseX::Types::Common
+
+=pkg MooseX::Types::DateTime
+
+=pkg MooseX::Types::Email
+
+=pkg MooseX::Types::LoadableClass
+
+=pkg MooseX::Types::Path::Class
+
+=pkg MooseX::Types::URI
+
+=pkg Net::Daemon
+
+=pkg Net::Server
+
+=pkg Net::Server::PreForkSimple
+
+=pkg Net::SSH
+
+=pkg Net::SSH::Expect
+
+=pkg Net::TFTP
+
+=pkg Parse::RecDescent
+
+=pkg Regexp::Common
+
+=pkg RPC::XML
+
+=pkg RPC::XML::Server
+
+=pkg SQL::Translator
+
+=pkg String::Diff
+
+=pkg Sub::Exporter
+
+=pkg TAP::DOM
+
+=pkg TAP::Formatter::HTML
+
+=pkg TAP::Parser::Aggregator
+
+=pkg Task::Catalyst
+
+=pkg Task::KiokuDB
+
+=pkg Task::Plack
+
+=pkg Template
+
+=pkg Term::ReadLine::Perl
+
+=pkg Test::Deep
+
+=pkg Test::Fixture::DBIC::Schema
+
+=pkg Test::MockModule
+
+=pkg Test::WWW::Mechanize::Catalyst
+
+=pkg TryCatch
+
+=pkg URI
+
+=pkg URI::Escape
+
+=pkg XML::Generator
+
+=pkg XML::Simple
+
+=pkg YAML
+
+=pkg YAML::Syck
+
+=pkg YAML::XS
+
+=cut
+
+1;
